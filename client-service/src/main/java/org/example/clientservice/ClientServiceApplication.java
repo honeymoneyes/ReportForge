415775e2b4a639a838e779b41e34dfce9c44415a
sanya <alejandroantoniuk@gmail.com>
2024-04-17 09:39:50 +0200

    changed names of modules

--- client-service/src/main/java/org/example/clientservice/ClientServiceApplication.java
+++ client-service/src/main/java/org/example/clientservice/ClientServiceApplication.java
@@ -1,8 +1,6 @@
 package org.example.clientservice;
 
 import lombok.RequiredArgsConstructor;
-import org.example.clientservice.config.Config;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
